eaf7787aa0bc9dc68fc917f5308ee74b8c967303
shahad <shahad.alrefaie@ryerson.ca>
2015-10-26 17:19:34 -0400

    new line

---  /lab5/src/lab5.cpp
+++  /lab5/src/lab5.cpp
@@ -15,6 +15,7 @@ int main() {
 	cout<<"hi all"<<endl;
 	cout<<"hi all"<<endl;
 	cout<<"hi all"<<endl;
-	cout<<"hi all"<<endl;// prints !!!Hello World!!!
+	cout<<"hi all"<<endl;
+	cout<<"shahad"<<endl;// prints !!!Hello World!!!
 	return 0;
 }
